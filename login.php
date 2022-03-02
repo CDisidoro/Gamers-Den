c9e1932c32c7873bc01e473701ef4c56d7a2cbc5
Dav16d <60506677+Dav16d@users.noreply.github.com>
2022-03-02 17:27:53 +0100

    Creación de login, logout y procesamiento login
    Esta sección está hecha por Arturo Lopez y David Cruza
    
    Cabe destacar que aún faltaría especificar un archivo donde declaremos los "usuarios" (En este archivo especificaremos los roles y sus distintas características) y también delcarar un archivo config donde gestionaremos la base de datos de la aplicación.

---
+++ login.php
@@ -0,0 +1,26 @@
+<!--Code Autor: David Cruza Sesmero-->
+
+<?php
+	// Seguramente solo necesitemos el archivo de config puesto que no necesitamos modelar ningún usuario aquí
+    //require_once __DIR__.'/includes/config.php';
+
+	$tituloPagina = 'Login'; // Titulo de la página actual
+	
+	$raizApp = RUTA_APP; // Definir la raiz de la app
+
+    // Capturamos mediante el parámetro <<< el contenido principal de la página en este caso un formulario de logueo y ponemos el delimitador EOS
+	$contenidoPrincipal=<<<EOS 
+	<h1>Acceso a Gamers Deen</h1>
+	
+	<form id="formLogin" action="${raizApp}/procesarLogin.php" method="POST"> 
+		<fieldset>
+			<legend>Usuario y contraseña</legend>
+			<div><label>Correo:</label> <input type="text" name="email" /></div>
+			<div><label>Password:</label> <input type="password" name="password" /></div>
+			<div><button type="submit">Entrar</button></div>
+		</fieldset>
+	</form>
+	EOS; // Delimitador EOS final
+	
+    // Volvemos a incluir el layout correspondiente al loggin
+	//require __DIR__.'/includes/comun/layout.php';
---
+++ logout.php
@@ -0,0 +1,24 @@
+<!--Code Autor: David Cruza Sesmero-->
+
+<?php
+    // Requires necesarios, seguramente puedan variar --> Veo necesarios uno referente a usuarios a su vez config
+
+	//require_once __DIR__.'/'directorio'/usuarios.php';
+	//require_once __DIR__.'/'directorio'/config.php';
+
+	$usuario=$_SESSION['nombre']; // Asocio a la variable usuario el usuario que esta utilizando la sesion actual
+	logout(); // Función de cierre de sesión
+
+	$tituloPagina = 'Logout'; // Título de la página 
+
+    // Capturamos la siguiente información mediante el operador <<< y saturamos la variable
+	$contenidoPrincipal=<<<EOS 
+		<main> // Captura de html que despide al usuario y muestra su nombre como uestro arriba
+			<article>
+				<h3>ESPERO QUE HAYAS DISFRUTADO DE LA EXPERIENCIA GAMERS DEEN!  </h3> {$usuario}
+			</article>
+		</main>
+	EOS; // Fin del delimitador EOS
+	
+    //Establezco la ruta seguramente necesaria para el layout
+	//require __DIR__.'/directorio/layout.php';
