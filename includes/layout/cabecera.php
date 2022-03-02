8c154363f8d06b3f6bb0124d12e7d024de6a7d16
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-02 18:16:03 +0100

    Añadir foto

---
+++ includes/layout/cabecera.php
@@ -0,0 +1,13 @@
+<img src="img/Noticiasgeneral.jpg" class="boceto" alt="Noticias"/>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Portada</title>
+</head>
+
+<body>
+<img src="img/Logo.jpg" class="Logo" alt="Cabecera"/>
+</body>
+</html>
