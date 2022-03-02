415dc1a66ab5e68ab46b60e55b097a6f8ebc3a44
luisma722 <josikooo69@gmail.com>
2022-03-02 18:17:31 +0100

    Merge branch 'master' of https://github.com/CDisidoro/Practica2-AW

---
+++ includes/layout/pie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+    </head>
+    <body>
+        <p>Bienvenido a Gamers Den, nuestra página podrá proporcionarte una lista perosnalizada de juegos a tu gusto.
+        Piensas que has acabado tu juego favorito y que ya no tienes nada más que hacer; pues se acabó, Gamers Den te
+        proporcionará una lista de videojuegos así como otras funcionalidades.
+        </p>
+    </body>
+</html>
