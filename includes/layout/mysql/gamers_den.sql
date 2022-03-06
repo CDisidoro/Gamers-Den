71d63ec82f97b434d0ebc9936d63536c753c3ea5
Camilo <cdisidoro99@gmail.com>
2022-03-06 13:42:22 +0100

    Cambios a BD

--- includes/layout/mysql/gamers_den.sql
+++ includes/layout/mysql/gamers_den.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 02-03-2022 a las 18:28:24
+-- Tiempo de generación: 06-03-2022 a las 13:40:40
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -24,6 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `ID` int(11) NOT NULL,
+  `Autor` int(11) NOT NULL,
+  `Foro` int(11) NOT NULL,
+  `Contenido` text NOT NULL,
+  `Fecha` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Comentarios en los foros';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `foro`
+--
+
+CREATE TABLE `foro` (
+  `ID` int(11) NOT NULL,
+  `Autor` int(11) NOT NULL,
+  `Contenido` text NOT NULL,
+  `Fecha` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juegos`
+--
+
+CREATE TABLE `juegos` (
+  `ID` int(11) NOT NULL,
+  `Nombre` text NOT NULL,
+  `Descripcion` text NOT NULL,
+  `Lanzamiento` date NOT NULL,
+  `Desarrollador` text NOT NULL,
+  `Precio` int(11) NOT NULL COMMENT 'Precio Oficial del Desarrollador'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `ID` int(11) NOT NULL,
+  `Remitente` int(11) NOT NULL,
+  `Destinatario` int(11) NOT NULL,
+  `Fecha` date NOT NULL DEFAULT current_timestamp(),
+  `Contenido` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda`
+--
+
+CREATE TABLE `tienda` (
+  `ID` int(11) NOT NULL,
+  `Vendedor` int(11) NOT NULL,
+  `Articulo` int(11) NOT NULL,
+  `Precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -33,13 +102,50 @@ CREATE TABLE `usuarios` (
   `Password` varchar(40) NOT NULL COMMENT 'SHA',
   `Email` text NOT NULL,
   `Rol` text NOT NULL,
-  `Avatar` int(11) DEFAULT NULL,
+  `Avatar` int(11) DEFAULT 1,
   `Biografia` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Autor` (`Autor`) USING BTREE,
+  ADD KEY `FK_Foro` (`Foro`) USING BTREE;
+
+--
+-- Indices de la tabla `foro`
+--
+ALTER TABLE `foro`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Autor` (`Autor`) USING BTREE;
+
+--
+-- Indices de la tabla `juegos`
+--
+ALTER TABLE `juegos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Remitente` (`Remitente`) USING BTREE,
+  ADD KEY `FK_Destinatario` (`Destinatario`) USING BTREE;
+
+--
+-- Indices de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Vendedor` (`Vendedor`) USING BTREE,
+  ADD KEY `FK_Articulo` (`Articulo`) USING BTREE;
 
 --
 -- Indices de la tabla `usuarios`
@@ -53,10 +159,71 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `foro`
+--
+ALTER TABLE `foro`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `juegos`
+--
+ALTER TABLE `juegos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`Autor`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`Foro`) REFERENCES `foro` (`ID`);
+
+--
+-- Filtros para la tabla `foro`
+--
+ALTER TABLE `foro`
+  ADD CONSTRAINT `foro_ibfk_1` FOREIGN KEY (`Autor`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`Remitente`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mensajes_ibfk_2` FOREIGN KEY (`Destinatario`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tienda`
+--
+ALTER TABLE `tienda`
+  ADD CONSTRAINT `tienda_ibfk_1` FOREIGN KEY (`Vendedor`) REFERENCES `usuarios` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tienda_ibfk_2` FOREIGN KEY (`Articulo`) REFERENCES `juegos` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
