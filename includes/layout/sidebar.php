5de41ae2688f8fb4b4aa6a7a8732e6ae05c79ec8
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-08 17:20:38 +0100

    Update sidebar.php

--- includes/layout/sidebar.php
+++ includes/layout/sidebar.php
@@ -1,8 +1,8 @@
 <div id = "sidebar">
-    <img src = "img/ImagenInicio.jpg">
-    <img src = "img/ImagenChat.jpg">
-    <img src = "img/ImagenPerfil.jpg">
-
+    <img src = "">
+    <a title="MenuInicio" href="index.php"><img src="img/ImagenInicio.jpg" class="menu"></a>
+    <a title="MenuPerfil" href="paginaPerfil.html"><img src="img/ImagenPerfil.jpg" class="menu"></a>
+    <a title="MenuChat" href="chat.php"><img src="img/ImagenChat.jpg" class="menu"></a>
 </div> <!-- Fin del sidebar -->
 </body>
 </html>
