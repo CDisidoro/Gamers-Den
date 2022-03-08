623e1f84b432fb5e3d8246883621a119e8b3bc67
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-08 11:45:39 +0100

    adding css

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
+<link rel="stylesheet" type="text/css" href="includes\layout\estilo.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Portada</title>
 </head>
