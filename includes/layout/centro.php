3258a3d17c43622d4a1e3c8dc831199118865fdd
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-08 11:50:57 +0100

    pepepepeep

--- includes/layout/centro.php
+++ includes/layout/centro.php
@@ -2,7 +2,7 @@
     <?php
         if (!isset($_SESSION["login"])) { //Usuario incorrecto
             echo "Página principal";
-            echo "<p>Aquí está el contenido público, visible para todos los usuarios..</p>";
+            echo "<p>Aquí está el contenido público, visible para todos los usuarios con o sin registro..</p>";
         }
         else { //Usuario registrado
             echo "<h1>Bienvenido {$_SESSION['nombre']}</h1>";
