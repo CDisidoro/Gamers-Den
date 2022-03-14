63e6359153f8da75aa237bc4cb735614fbfc8da4
luisma722 <josikooo69@gmail.com>
2022-03-14 13:13:06 +0100

    imagenes

--- includes/layout/sidebar.php
+++ includes/layout/sidebar.php
@@ -1,8 +1,8 @@
 <button>
     <div id = "sidebar">
         <img src = "">
-        <a title="MenuInicio" href="index.php"><img src="img/ImagenInicio1.jpg" class="menu"></a>
-        <a title="MenuPerfil" href="paginaPerfil.php"><img src="img/ImagenPerfil1.jpg" class="menu"></a>
-        <a title="MenuChat" href="chat.php"><img src="img/ImagenChat1.jpg" class="menu"></a>
+        <a title="MenuInicio" href="index.php"><img src="img/imagenInicio.jpg" class="menu"></a>
+        <a title="MenuPerfil" href="paginaPerfil.php"><img src="img/imagenPerfil.jpg" class="menu"></a>
+        <a title="MenuChat" href="chat.php"><img src="img/imagenChat.jpg" class="menu"></a>
     </div> <!-- Fin del sidebar -->
 </button>
