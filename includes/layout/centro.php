407c4b1e6219604eee50e49720853ca4e5f0c977
luisma722 <josikooo69@gmail.com>
2022-03-16 18:28:47 +0100

    centro.php

--- includes/layout/centro.php
+++ includes/layout/centro.php
@@ -1,12 +1,13 @@
 <main id="contenido">
     <?php
         if (!isset($_SESSION["login"])) { //Usuario incorrecto
-            echo "Página principal";
-            echo "<p>Aquí está el contenido público, visible para todos los usuarios con o sin registro..</p>";
+            echo "<h1>Bienvenido a la página principal de Gamers Den!!!</h1>";
+            echo "<p>Aquí está el contenido público, visible para todos los usuarios con o sin registro.</p>";
+            echo "<p>Usa la barra de menú para navegar.</p>";
         }
         else { //Usuario registrado
             echo "<h1>Bienvenido {$_SESSION['nombre']}</h1>";
-            echo "<p>Usa el menú de la izquierda para navegar.</p>";
+            echo "<p>Usa la barra de menú para navegar.</p>";
         }
     ?>
 </main>
