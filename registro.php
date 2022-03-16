ebda7cce40c2128dacc3dd66d75ae7b9b5958012
Dav16d <60506677+Dav16d@users.noreply.github.com>
2022-03-16 22:53:28 +0100

    Registro y Registro en la bd
    En registro tenemos un formulario de registro en el que pide al usuario su nombre de usuario, contraseña (y otra para "confirmación") y email; creo haber incluido correctamente los paths que a diferencia por ejemplo de los de loging (que había muchos que como los hice antes de tiempo por ejemplo ponía usuariosBD y no usuariosbd) entonces no cogía nada.
    
    Adjunto los sitios donde he podido ver/sacar código de como se hace (más lo de condiciones de servicio que me ha hecho gracia):
        ## Código del formulario: https://www.php.net/manual/es/tutorial.forms.php
        ## Términos de la página: https://www.htmlquick.com/es/reference/tags/input-checkbox.html 
    
    Por último tenemos registro_exito.php donde aquí checkeamos y mostramos al user si su registro ha tenido éxito, nos valemos de la función altaNuevoUsuario la cual se encuentra en usuariobd; esta función mira principalmente si password1==password2 si esto es así se dispone a meter en la bd tanto el email como el nombre del usuario como su contraseña --> Por defecto no tiene ni logo, ni descripción y los roles de escritor y admin estan a 0.
    
    Me voy a dormir ya que se me caen las pestañas, ECHADLE UN VISTAZO!! :D

---
+++ registro.php
@@ -0,0 +1,39 @@
+<?php
+	require_once __DIR__.'/includes/config.php'; ## Rutas necesarias para establecer la bd, en este caso introducción de usuarios
+	require_once __DIR__.'/includes/usuariobd.php'; ## Funciones referentes al usuariobd
+
+	## Mostramos el registro con un formulario en el que se nos pide: Nombre, email y contraseña
+	$tituloPagina = 'Registrandose en Gamers Den';
+
+    ## Capturamos todo el html mediante el operador multilinea <<<
+    ## Código del formulario: https://www.php.net/manual/es/tutorial.forms.php
+    ## Términos de la página: https://www.htmlquick.com/es/reference/tags/input-checkbox.html
+
+	$contenidoPrincipal=<<<EOS 
+	
+	<div id="contenedor">	
+		<main>
+		<article>
+				<h1>Registro de usuario</h1>
+				<form method="get" action="registroUsuariobd.php">
+				Usuario:
+				<input type="text" name="Username"/>
+			<br/>
+				Correo:
+				<input type="text" name="email"/>
+			<br/>
+				Contraseña:
+				<input type="text" name="password1"/>
+			<br/>
+				Confirmar contraseña:
+				<input type="text" name="password2"/>
+			<br/>
+				<input type="checkbox" name="cb-terminosservicio" required> He leído y acepto los términos del servicio<br>
+				<input type="submit" value="crear">
+				</form>
+			</article>
+		</main>
+	</div>
+	EOS;
+
+require __DIR__.'/includes/layout/centro.php'; ## Require del doc centro.php que es donde pondremos la info
