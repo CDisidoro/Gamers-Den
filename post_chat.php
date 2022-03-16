21b580ef095a1f16900add83d9a5f0f3f8dad764
Dav16d <60506677+Dav16d@users.noreply.github.com>
2022-03-16 17:53:42 +0100

    Update post_chat.php
    He cambiado el nombre de la variable de text a mensaje porque en la base de datos los mensajes de un usuario esta en el campo "mensajes"

--- post_chat.php
+++ post_chat.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['name'])){ ## Cogemos el nombre del usuario que está loguead
         ## Podríamos establecer un echo para decirle al usuario que esta poniendo 'nada'
     }
 	else {
-        $text = $_POST['text']; ## Pasamos lo que ha escrito el user a la variable text
+        $text = $_POST['mensajes']; ## Pasamos lo que ha escrito el user a la variable text
         $fp = fopen("log.html", 'a'); ## Abrimos el archivo log y volcamos lo que escribe el user
         fwrite($fp, "<div class='msgln'><span>(".date("g:i A").") <b><user>".$_SESSION['name']."</user></b>: ".stripslashes (htmlspecialchars($text))."<br></span></div>");
         fclose($fp);
