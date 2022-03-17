7079c857b5469c1fea9621805349c7a95352504d
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-17 20:18:44 +0100

    notfound.php

--- includes/vistas/comun/sidebar.php
+++ includes/vistas/comun/sidebar.php
@@ -26,31 +26,31 @@
 
                     <li>
                         <div class = "cajaforoNav">
-                            <a href = "notfound.html"> Foro</a>
+                            <a href = "notfound.php"> Foro</a>
                         </div>               
                     </li>
 
                     <li>
                         <div class = "cajanoticiasNav">
-                            <a href = "notfound.html" > Noticias</a>
+                            <a href = "notfound.php" > Noticias</a>
                         </div>               
                     </li>
 
                     <li>
                         <div class = "cajacalendarioNav">
-                            <a href = "notfound.html" > Calendario</a>
+                            <a href = "notfound.php" > Calendario</a>
                         </div>                        
                     </li>
 
                     <li>
                         <div class = "cajatiendaNav">
-                            <a href = "notfound.html" > Tienda</a>
+                            <a href = "notfound.php" > Tienda</a>
                         </div>                    
                     </li>
 
                     <li>
                         <div class = "cajatusjuegosNav">
-                            <a href = "notfound.html" > Tus Juegos</a>
+                            <a href = "notfound.php" > Tus Juegos</a>
                         </div>                       
                     </li>
                 </ul>
