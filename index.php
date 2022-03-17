4cc71657c5c02472d98cbaaa5a259c50c4f27f46
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-17 16:43:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" type="text/css" href="<?=RUTA_CSS.'/estilo.css'?>" />
+		<link rel="stylesheet" type="text/css" href="<?='css/estilo.css'?>" />
 		<title><?= $tituloPagina ?></title>
 	</head>
 	<body>
 		<div id="contenedor">
 			<?php
-				require(_DIR_.'/cabecera.php');
-				require(_DIR_.'/sidebar.php');
+				require('includes/vistas/comun/cabecera.php');
+				require('includes/vistas/comun//sidebar.php');
 			?>
 			<main>
 				<article>
 					<?= $contenidoPrincipal ?>
 				</article>
 			</main>
-			<?php require(_DIR_.'/pie.php');?>
+			<?php require('includes/vistas/comun//pie.php');?>
 		</div>
 	</body>
 </html>
