754948145357dd9f15fecc315918379ddd40483e
Arturo1703 <arturolopez01.al@gmail.com>
2022-03-17 16:33:06 +0100

    nuevo index

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php namespace es\fdi\ucm\aw\gamersDen;?>
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Portada</title>
-</head>
-
-<body>
-<div id="contenedor">
-
-	<?php
-		session_start();
-		require ('includes/vistas/comun/cabecera.php');
-		require ('includes/vistas/comun/sidebar.php');
-		require('includes/vistas/comun/centro.php');
-		require ('includes/vistas/comun/pie.php');
-	?>
-
-</div> <!-- Fin del contenedor -->
-
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="<?=RUTA_CSS.'/estilo.css'?>" />
+		<title><?= $tituloPagina ?></title>
+	</head>
+	<body>
+		<div id="contenedor">
+			<?php
+				require(_DIR_.'/cabecera.php');
+				require(_DIR_.'/sidebar.php');
+			?>
+			<main>
+				<article>
+					<?= $contenidoPrincipal ?>
+				</article>
+			</main>
+			<?php require(_DIR_.'/pie.php');?>
+		</div>
+	</body>
 </html>
