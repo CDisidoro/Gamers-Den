87d8cb76e057cbfad0470ce2646bd39d64b5a18b
Antonio <antoboni@ucm.es>
2022-03-17 17:26:30 +0100

    Cambio en index.php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
 <?php namespace es\fdi\ucm\aw\gamersDen;?>
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-<head>
-<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Portada</title>
-</head>
-
-<body>
-<div class ="contenedor">
-	<?php 
-		session_start();
-        require ('includes/vistas/comun/cabecera.php');
-    ?>
-          
-	<div class = "clearfix"> </div>
-         
-    <?php 
-        require ('includes/vistas/comun/sidebar.php');
-    ?>   
-
-    <div class = "clearfix"></div>
-	
-	<section>
-		<?php
-			require ('includes/vistas/comun/centro.php');
-		?>
-	</section>
-
-	<?php
-		require ('includes/vistas/comun/pie.php');
-	?>
-
-</div> <!-- Fin del contenedor -->
-
-</body>
-=======
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="<?='css/estilo.css'?>" />
@@ -55,6 +19,4 @@
 			</main>
 			<?php require('includes/vistas/comun//pie.php');?>
 		</div>
-	</body>
->>>>>>> 4cc71657c5c02472d98cbaaa5a259c50c4f27f46
 </html>
