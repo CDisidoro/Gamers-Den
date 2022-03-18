09d817e315bfe9850fb384588b40b6be0ee2b5c8
Dav16d <60506677+Dav16d@users.noreply.github.com>
2022-03-18 15:45:51 +0100

    Errores en post_chat
    Un poco igual como con chat sobretodo que el texto que escribía el usuario lo recogía en una variable texto cuando la variable que hay respecto a los mensajes se llama Contenido.

--- post_chat.php
+++ post_chat.php
@@ -1,14 +1,14 @@
 <?php namespace es\fdi\ucm\aw\gamersDen;
-session_start(); ## Creamos o reestablecemos la sesiñon
+session_start(); ## Creamos o reestablecemos la sesion
 
-if(isset($_SESSION['name'])){ ## Cogemos el nombre del usuario que está logueado
-    if ($_POST ['text'] == "") {
+if(isset($_SESSION['Usuario'])){ ## Cogemos el nombre del usuario que está logueado
+    if ($_POST ['Contenido'] == "") {
         ## Podríamos establecer un echo para decirle al usuario que esta poniendo 'nada'
     }
 	else {
-        $text = $_POST['mensajes']; ## Pasamos lo que ha escrito el user a la variable text
+        $text = $_POST['Contenido']; ## Pasamos lo que ha escrito el user a la variable text
         $fp = fopen("log.html", 'a'); ## Abrimos el archivo log y volcamos lo que escribe el user
-        fwrite($fp, "<div class='msgln'><span>(".date("g:i A").") <b><user>".$_SESSION['name']."</user></b>: ".stripslashes (htmlspecialchars($text))."<br></span></div>");
+        fwrite($fp, "<div class='msgln'><span>(".date("g:i A").") <b><user>".$_SESSION['Usuario']."</user></b>: ".stripslashes (htmlspecialchars($text))."<br></span></div>");
         fclose($fp);
     }
 }
