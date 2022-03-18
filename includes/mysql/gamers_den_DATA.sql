c3744df1fd552f210aeb1981b0ba6538a8e23108
Camilo <cdisidoro99@gmail.com>
2022-03-18 11:27:55 +0100

    SQL con datos

---
+++ includes/mysql/gamers_den_DATA.sql
@@ -0,0 +1,11 @@
+TRUNCATE TABLE 'usuarios';
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+-- admin: adminpass
+-- escritor: escritorpass
+-- usuario: userpass
+INSERT INTO `usuarios` (`ID`, `Usuario`, `Password`, `Email`, `Rol`, `Avatar`, `Biografia`) VALUES
+(1, 'admin', '$2y$10$Tqw1LUHWQ7kCSaC/HbwW..8mc/lacMnohhtQPJFLI5UNSDnZVul96', 'admin@gamersden.com', '1', 1, 'Juego o saco el banhammer?'),
+(2, 'escritor', '$2y$10$ZTNUvjtNUGIih/o2lhoNh.pF7enWI2yiU9s.MdiYjgS/m3iA6rouS', 'escritor@gamersden.com', '2', 1, 'Ya habéis leído que salió el Elden Ring?'),
+(3, 'usuario', '$2y$10$G9IeXOVfmIxO1akx/CJNH.fabWr4tPyKjTuvY/bEx06kE0z9UF7WO', 'usuario@gmail.com', '3', 1, NULL);
