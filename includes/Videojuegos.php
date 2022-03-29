3cf56132c4fa3143f90ab25cc64a4fc1a96b5eb6
Dav16d <60506677+Dav16d@users.noreply.github.com>
2022-03-29 17:26:27 +0200

    Videojuegos
    Que hace/tiene videojuegos por ahora:
    
    - Tiene la clase de videojuegos con los distintos atributos que recogemos en la base de datos.
    - Tiene las funciones de cargarVideojuegos (Selecciona todos los juegos y atributos; los devuelve en un array de productos), subeVideojuego (Recoge los datos necesarios para introducir el juego en la bd) y buscarJuego (Busca un determinado juego en la bd según su ID, esto creo que es mejor que que lo busque con el nombre).
    - Distintos getters utiles.
    
    FALTA:
    - Determinar lo de la imagen de los videojuegos y la función a su vez de mostrarImagen
    - Implementar botones de post con su formulario para subir los videojuegos
    - Distintos métodos para ordenar los videojuegos: Nombre (A-Z/Z-A), Año de salida, etc... (O incluso vinculaciones por género, esto sería curioso para luego recomendar videojuegos al usuario).
    - A lo mejor un modelo de cuadrículas?? para enseñar los juegos (En plan algo que se aplique con estilos css para que se muestren los juegos distribuidos como si estuvieran en celda de matrices y solamente se viese la imagen del juego y el nombre y al clickar en el nombre te lleve a una página donde te expliquen ese videojuego en profundidad)

---
+++ includes/Videojuegos.php
@@ -0,0 +1,105 @@
+<?php namespace es\fdi\ucm\aw\gamersDen;
+
+class Videojuego{ ## Estos son los atributos que tenemos puestos en la bd, excepto la bd
+	private $id;
+	private $nombre;
+	private $descripcion;
+	private $lanzamiento;
+	private $desarrollador;
+	private $urlImagen;
+	private $precio;
+	
+	function __construct($id, $nombre, $descripcion, $desarrollador, $lanzamiento, $precio) {
+		$this->id = $id;
+		$this->nombre = $nombre;
+		$this->descripcion = $descripcion;
+		$this->lanzamiento = $lanzamiento;
+		$this->precio = $precio;
+		$this->desarrollador = $desarrollador;
+		//$this->urlImagen = $urlImagen; ## no tenemos asociadas imagenes a los videojuegos??
+	}
+
+	// Cuando incluyamos la imagen hay que tenerla en cuenta en las distintas funcionalidades
+	
+	public static function cargarVideojuegos(){
+		$mysqli = getConexionBD();
+		$query = sprintf("SELECT * FROM juegos");
+		$result = $mysqli->query($query);
+
+		$ofertasArray;
+		
+		if($result) {
+			for ($i = 0; $i < $result->num_rows; $i++) {
+				$fila = $result->fetch_assoc();
+				$ofertasArray[] = new Videojuego($fila['ID'],$fila['Nombre'],$fila['Descripcion'],
+									$fila['Lanzamiento'],$fila['Desarrollador'],$fila['Precio']);		
+			}
+			return $ofertasArray;
+		}
+		else{
+			echo "Error in ".$query."<br>".$mysqli->error;
+		}
+	}
+	
+	public static function subeVideojuego() {
+		$nombre = htmlspecialchars(trim(strip_tags($_POST["NombreJuego"])));
+		$descripcion = htmlspecialchars(trim(strip_tags($_POST["descripcionJuego"])));
+		$lanzamiento = htmlspecialchars(trim(strip_tags($_POST["LanzamientoJuego"])));
+		$desarrollador = htmlspecialchars(trim(strip_tags($_POST["DesarrolladorJuego"])));
+		$precio = htmlspecialchars(trim(strip_tags($_POST["precioJuego"])));
+		
+		$mysqli = getConexionBD();
+		
+		$sql = "INSERT INTO juegos (Nombre, Descripcion, Lanzamiento, Desarrollador, Precio)
+				VALUES ('$nombre', '$descripcion', '$lanzamiento', '$desarrollador', '$precio')";
+		
+		if (mysqli_query($mysqli, $sql)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static function buscaVideojuego($id) {
+		$mysqli = getConexionBD();
+		$query = "SELECT * FROM juegos WHERE ID = '$id'";
+		$result = $mysqli->query($query);
+		
+		if($result) {
+			$fila = $result->fetch_assoc();
+			$buscaJuego = new Videojuego($fila['ID'],$fila['Nombre'],$fila['Descripcion'],
+									$fila['Lanzamiento'],$fila['Desarrollador'],$fila['Precio']);
+			return $buscaJuego;
+		} else{
+			echo"No se ha encontrado el videojuego";
+			return false;
+		}
+	}
+	
+
+	public function muestraID() {
+		return $this->id;
+	}
+	
+	public function muestraNombre() {
+		return $this->nombre;
+	}
+	public function muestraDescripcion() {
+		return $this->descripcion;
+	}
+	
+	public function muestraLanzamiento() {
+		return $this->lanzamiento;
+	}
+	
+	public function muestraDesarrollo() {
+		return $this->Desarrollo;
+	}
+	
+	public function muestraPrecio() {
+		return $this->precio;
+	}
+	
+  }
+?>
