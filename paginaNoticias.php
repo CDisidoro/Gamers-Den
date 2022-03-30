6fea4a014ed5bedc01da8b6ddcc6970c326f5110
JesusAbajo <j.abajo2001@gmail.com>
2022-03-30 17:49:51 +0200

    paginanoticias inicio de la css

--- paginaNoticias.php
+++ paginaNoticias.php
@@ -2,61 +2,19 @@
 	require('includes/config.php');
     $tituloPagina = "Noticias";
     if(isset($_SESSION['login'])){
-        $username = $_SESSION['Usuario'];
-        $id = $_SESSION['ID'];    
-        $usuario = Usuario::buscarUsuario($username);
-        $bio = $usuario->getBio();
-
-        function generaAmigos($usuario){
-            $htmlAmigos = '';
-            $amigos = $usuario->getfriendlist();
-            $index = 0;
-            while($index < sizeof($amigos[1])){
-                $srcAvatar = 'img/avatar';
-                $srcAvatar .= $amigos[1][$index];
-                $srcAvatar .= '.jpg';
-
-                $htmlAmigos .= '<div class = "amigolista">';
-                $htmlAmigos .= '<img class = "avatarPerfilUsuario" src = "';
-                $htmlAmigos .= $srcAvatar;
-                $htmlAmigos .= '">';
-                $htmlAmigos .= '<p class = "nombreamigo">';
-                $htmlAmigos .= $amigos[0][$index];
-                $htmlAmigos .= '</p>';
-                $htmlAmigos .= '</div>';
-                $index++;
-            }        
-            return $htmlAmigos;
-        }
-
-        function generaAvatar($usuario){
-            $srcAvatar = 'img/avatar';
-            $srcAvatar .= $usuario->getAvatar();
-            $srcAvatar .= '.jpg';
-    
-            $htmlAvatar = '';
-            $htmlAvatar .= '<img class = "avatarPerfilUsuario" src = "';
-            $htmlAvatar .= $srcAvatar;
-            $htmlAvatar .= '">';
-            return $htmlAvatar;
-        }
-
-        $htmlAvatar = generaAvatar($usuario);
-        $htmlAmigos = generaAmigos($usuario);
+       
 
         $contenidoPrincipal=<<<EOS
             <section class = "noticiasPrincipal">
-                <article class = "avatarydatos">
-                    <p> Prueba </p>
-                </article>
+                <div class = "prueba">
+                    <div class = "noticiaDestacada">
+                        <p class ="noticiaDestacadaP" > NOTICIAS </p>
+                    </div>
+                </div>
 
-                <article class = "listadeseados">
-                    
-                </article>
-                
-                <article class = "listadeamigos">
-                    
-                </article>
+                <div  class = "prueba">
+                <p> NOTICIAS 2</p>
+                </div>
             </section>
         EOS;
     }else{
