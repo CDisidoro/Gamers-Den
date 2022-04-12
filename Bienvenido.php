b0722017de390e4b7b757a86f03bf1b65a1e36c5
luisma722 <josikooo69@gmail.com>
2022-04-12 13:17:02 +0200

    cambios

---
+++ Bienvenido.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>GAMERS DEN</title>
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+</head>
+
+<body>
+    <script type = "text/javascript">
+        $(document).ready(function() {
+            swal({
+                title: "Bienvenido a GAMERS DENS!",
+                text: "Espero que te guste nuestra página web!!",
+                icon: "img/Logo.jpg",
+                button: "Ok",
+            });
+        });
+    </script>
+</body>
+</html>
