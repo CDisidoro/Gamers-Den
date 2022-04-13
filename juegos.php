e24a97997bbf4a53c5896520f7d560e241371e8f
Camilo <cdisidoro99@gmail.com>
2022-04-13 11:14:37 +0200

    Se muestran los juegos de la BD en una vista general

--- juegos.php
+++ juegos.php
@@ -1,14 +1,50 @@
 <?php namespace es\fdi\ucm\aw\gamersDen;
 	require('includes/config.php');
     $tituloPagina = 'Juegos';
-
-    $contenidoPrincipal = <<<EOS
-        <section class="content">
-            <article>
-                <h2>Descubre juegos nuevos</h2>
-            </article>
-            <p>Aqui va a estar toda la funcionalidad de los juegos</p>
-        </section>
-    EOS;
+    function generaJuegos(){
+        $arrayJuegos = Videojuego::cargarVideojuegos();
+        $juegos = '';
+        foreach($arrayJuegos as $juego){
+            $idJuego = $juego->getID();
+            $nombreJuego = $juego->getNombre();
+            $descJuego = $juego->getDescripcion();
+            if(strlen($descJuego) > 260){
+                $descJuego = substr($descJuego, 0, 260);
+                $descJuego .= '...';
+            }
+            $imgJuego = $juego->getUrlImagen();
+            $redir = 'juego_particular.php?id='.$idJuego;
+            $juegos .= <<<EOS
+                <div class="tarjetaProducto">
+                    <a href="$redir">
+                        <img src=$imgJuego class="imagenTajetaProducto"/>
+                        <p class="nombreProductoTarjeta">$nombreJuego</p>
+                    </a>
+                    <p class="descripcionProductoTarjeta">$descJuego</p>
+                </div>
+            EOS;
+        }
+        return $juegos;
+    }
+    if(isset($_SESSION['login'])){
+        $listaJuegos = generaJuegos();
+        $contenidoPrincipal = <<<EOS
+            <section class="content">
+                <div class="contenedorProductos">
+                    <h1 class="tituloPagina">Descubre juegos nuevos</h1>
+                </div>
+                <div class="botonesProductos">
+                    <div class ="cajaBusqueda">
+                        <a href="buscarJuego.php"><img src="img/lupa.png" class="imagenBusqueda"/></a>
+                    </div>
+                </div>
+                <div class="cuadroProductos">
+                    $listaJuegos
+                </div>
+            </section>
+        EOS;
+    }else{
+        $contenidoPrincipal = "<p>No has iniciado sesión. Por favor logeate para ver los juegos disponibles</p>";
+    }
     include 'includes/vistas/plantillas/plantilla.php';
 ?>
