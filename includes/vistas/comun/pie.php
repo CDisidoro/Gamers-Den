6ca9f6760efc7e2c08c47b8a9328d948c34c755b
luisma722 <josikooo69@gmail.com>
2022-05-10 14:06:08 +0200

    footer

--- includes/vistas/comun/pie.php
+++ includes/vistas/comun/pie.php
@@ -1,6 +1,34 @@
-<footer>
-	<p>Bienvenido a Gamers Den, nuestra página podrá proporcionarte una lista perosnalizada de juegos a tu gusto.
-	Piensas que has acabado tu juego favorito y que ya no tienes nada más que hacer; pues se acabó, Gamers Den te
-	proporcionará una lista de videojuegos así como otras funcionalidades.
-	</p>
-</footer>
+<?php namespace es\fdi\ucm\aw\gamersDen;
+?>
+<!DOCTYPE html>
+	<!--Iconos redes sociales -->
+	<script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+	<footer class = "footer">
+        <div class = "grupo1">
+            <div class="box">
+                <figure>
+                    <img src="img/Logo.jpg" alt="...">
+                </figure>
+            </div>
+            <div class = "box">
+				<h2>SOBRE NOSOTROS</h2>
+				<p>Bienvenido a Gamers Den, nuestra página podrá proporcionarte una lista personalizada de juegos a tu gusto.
+				Piensas que has acabado tu juego favorito y que ya no tienes nada más que hacer; pues se acabó, Gamers Den te
+				proporcionará una lista de videojuegos así como otras funcionalidades.
+				</p>
+            </div>
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+					<a href="https://www.instagram.com/gamersden.aw/" class="fa fa-instagram"></a>
+					<a href="https://twitter.com/GamersDen19" class="fa fa-twitter"></a>
+					<a href="https://www.youtube.com/channel/UCL9hwt_if0zy-9BSh-jGTng" class="fa fa-youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo2">
+			<p> © 2021 <b>Gamser Den</b> - Todos los derechos reservados</p>
+        </div>
+    </footer>
+</html>
